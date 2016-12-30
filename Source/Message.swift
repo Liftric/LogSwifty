3cdc4e586c00970a17d2d9f7e09badef9a841cda
Ben John <github@benjohn.de>
2016-12-30 09:02:07 +0100

    Refactored computation of descriptions.

--- Source/Message.swift
+++ Source/Message.swift
@@ -17,19 +17,44 @@ public enum Level: Int {
 }
 
 public struct Message: CustomStringConvertible {
-    public let description: String
+    public var description: String {
+        // deal with multiline
+        var stack = [String]()
+        message.enumerateLines { line, _ in
+            let stringRepresentable = "\(self.metadata) \(line)"
+            stack.append(stringRepresentable)
+        }
+        return stack.joined(separator: "\n")
+    }
 
     let metadata: Metadata
     let message: String
 
     init(_ body: [Any?], level: Level, file: String, function: String, line: Int) {
         self.metadata = Metadata(level: level, file: file, function: function, line: line)
-        self.message = Formatter.createMessage(body: body)
-        self.description = Formatter.createDescription(message: self.message, metadata: self.metadata)
+
+        // deal with variadic parameter
+        var unvariadic: String = ""
+        for b in body {
+            if let unwrapped = b {
+                unvariadic += "\(unwrapped) "
+                continue
+            }
+            unvariadic += "\(b) "
+        }
+        self.message = String(unvariadic.characters.dropLast())
     }
 
     public struct Metadata: CustomStringConvertible {
-        public let description: String
+        public var description: String {
+            let desc = [
+                "\(self.timestamp)",
+                "[\(self.level)]",
+                "\(self.file):\(self.line)",
+                "\(self.function):"
+            ]
+            return desc.joined(separator: "\t")
+        }
 
         let level: Level
         let file: String
@@ -42,40 +67,6 @@ public struct Message: CustomStringConvertible {
             self.file = URL(fileURLWithPath: file).lastPathComponent
             self.function = function
             self.line = line
-
-            // create description
-            let prefix = [
-                "\(self.timestamp)",
-                "[\(self.level)]",
-                "\(self.file):\(self.line)",
-                "\(self.function):"
-            ]
-            self.description = prefix.joined(separator: "\t")
-        }
-    }
-
-    struct Formatter {
-        static func createMessage(body: [Any?]) -> String {
-            // deal with variadic parameter
-            var unvariadic: String = ""
-            for b in body {
-                if let unwrapped = b {
-                    unvariadic += "\(unwrapped) "
-                    continue
-                }
-                unvariadic += "\(b) "
-            }
-            return String(unvariadic.characters.dropLast())
-        }
-
-        static func createDescription(message: String, metadata: Metadata) -> String {
-            // deal with multiline
-            var stack = [String]()
-            message.enumerateLines { line, _ in
-                let stringRepresentable = "\(metadata) \(line)"
-                stack.append(stringRepresentable)
-            }
-            return stack.joined(separator: "\n")
         }
     }
 }

7572045981fee1f1348b1db45b89730fdbd5be94
Ben John <github@benjohn.de>
2017-12-23 11:47:28 +0100

    Small explanation for using [Any?] instead of [Any]

--- Source/Message.swift
+++ Source/Message.swift
@@ -11,13 +11,16 @@ import Foundation
 public struct Message {
     let metadata: Metadata
     let message: String
-
+    
     init(_ body: [Any?], level: Level, file: String, function: String, line: Int) {
         self.metadata = Metadata(level: level, file: file, function: function, line: line)
-
+        
         // deal with variadic parameter
         var unvariadic: String = ""
         for b in body {
+            // the reason for having `[Any?]` and unwrapping here is that otherwise
+            // it would lead to debug messages like `Optional(...)` even though we
+            // do not have any optionals.
             if let unwrapped = b {
                 unvariadic += "\(unwrapped) "
                 continue
